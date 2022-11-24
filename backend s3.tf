a19cbac052ee61a16ed0e59bb6c3ca69bf1aeb40
amanze123 <89152754+amanze123@users.noreply.github.com>
2022-11-24 00:09:09 +0000

    Update backend s3.tf

--- backend s3.tf
+++ backend s3.tf
@@ -5,21 +5,33 @@
 #   region = "eu-west-2"
 #   }
 # }
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#     }
+#     random = {
+#       source = "hashicorp/random"
+#     }
+#   }
   
-  backend "remote" {
-  organization = "sockshop"
+#   backend "remote" {
+#   organization = "sockshop"
     
-    workspaces {
-      name = "sockshop"
+#     workspaces {
+#       name = "sockshop"
+#     }
+#   }
+# }
+
+terraform {
+      backend "remote" {
+        # The name of your Terraform Cloud organization.
+        organization = "sockshop"
+
+        # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+          name = "sockshop"
+        }
+      }
     }
-  }
-}
